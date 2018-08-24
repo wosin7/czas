4e3277edda17f33d8374f0fadd11537aeb94d47d
przemek <wosin7@gmail.com>
2018-08-24 22:36:15 +0200

    poprawki

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,5 +1,6 @@
 var os = require('os');
 var timer = require('../modules/time');
+var tabs = [];
 function getOSinfo() {
     var type = os.type();
     if(type === 'Darwin') {
@@ -7,13 +8,19 @@ function getOSinfo() {
     } else if(type === 'Windows_NT') {
         type = 'Windows';
     }
+    var up = os.uptime()
+    tabs = timer.print(up);
+    /* var time = timer.print(up);
+    console.log('aaaaa');
+    console.log(timer.print(up));
+    console.log('bbb'); */
     var release = os.release();
     var cpu = os.cpus()[0].model;
     var userInfo = os.userInfo();
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    timer.print()
+    console.log('Uptime: ' +tabs[0]+' godzin, '+tabs[1]+' minut, '+tabs[2]+' sekund.');
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
--- modules/time.js
+++ modules/time.js
@@ -1,26 +1,31 @@
-var os = require('os');
-function timer() {
-var uptime = os.uptime();
+//var os = require('os');
+function timer(up) {
+var uptime = up;
 var seconds;
 var minut;
 var hour;
-if (uptime < 3600){
+var tab =[];
+/* if (uptime < 3600){
    // console.log('w ifie');
     hour = 0;
     minut = Math.floor(uptime / 60);
     seconds = Math.floor(uptime % 60);
 } 
-else {
+else { */
    // console.log('w elsie');
     hour = Math.floor(uptime / 3600);
     minut = Math.floor((uptime % 3600)/60);
     seconds = Math.floor((uptime % 3600) % 60)
-}
+    tab[0] = hour;
+    tab[1] = minut;
+    tab[2] = seconds;
+    return tab;
+/* }
 if (hour > 0){
-    console.log('Uptime: '+hour+' godzin, '+minut+' minut, '+seconds+' sekund.');
+    console.log(hour+' godzin, '+minut+' minut, '+seconds+' sekund.');
 } 
 else {
-    console.log('Uptime: '+minut+' minut, '+seconds+' sekund.');
-}
+    console.log(minut+' minut, '+seconds+' sekund.');
+} */
 }
 exports.print = timer;
